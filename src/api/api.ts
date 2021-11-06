c75b2608674db43281cd19e4c2fe7ea8ed146ad1
Valter <vcvijic@protonmail.com>
2021-11-06 13:53:19 +0100

    create api class

---
+++ src/api/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { Match, Tournament } from "../types";
+
+export class Api {
+  baseURL = `${process.env.REACT_APP_API_BASE_URL}`;
+
+  async getTournaments(): Promise<Array<Tournament>> {
+    const response = await axios.get(`${this.baseURL}/tournaments`);
+    return response.data;
+  }
+
+  async getMatches(id: number): Promise<Array<Match>> {
+    const response = await axios.get(`${this.baseURL}/matches/${id}`);
+    return response.data;
+  }
+}
