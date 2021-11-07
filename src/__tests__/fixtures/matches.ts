28b33c8ffe817056552355b4c1ea235cc1729915
Valter <vcvijic@protonmail.com>
2021-11-07 15:28:52 +0100

    add matches spec fixtures

---
+++ src/__tests__/fixtures/matches.ts
@@ -0,0 +1,22 @@
+import { Match } from "../../types";
+
+export const match: Array<Match> = [
+  {
+    id: "26548996",
+    time: 1621695600,
+    result: {
+      home: 4,
+      away: 0,
+      period: "nt",
+      winner: "home",
+    },
+    homeTeam: "FC Salzburg",
+    awayTeam: "WSG Tirol",
+    events: [
+      "1:0 (8.) Bernardo",
+      "2:0 (26.) M.Berisha",
+      "3:0 (44.) M.Berisha (pen)",
+      "4:0 (66.) M.Berisha",
+    ],
+  },
+];
